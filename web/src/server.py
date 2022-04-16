3941c07745e0de1c3132ed041e9080bcf791d88a
wsx2021 <80902225+wsx2021@users.noreply.github.com>
2022-04-16 15:50:13 -0700

    Update server.py

--- web/src/server.py
+++ web/src/server.py
@@ -1,7 +1,7 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.renderers import render_to_response
-
+from pyramid.response import FileResponse
 import mysql.connector as mysql
 import os
 
@@ -22,8 +22,13 @@ def get_home(req):
 
 def get_kvp(req):
   # Connect to the database and retrieve the users
-  return render_to_response('templates/kvp.html')
-  
+  db = mysql.connect(host=db_host, database=db_name, user=db_user, passwd=db_pass)
+  cursor = db.cursor()
+  cursor.execute("select first_name, last_name, position, description from Users;")
+  records = cursor.fetchall()
+  db.close()
+
+  return render_to_response('templates/kvp.html', {'users': records}, request=req)
 
 ''' Route Configurations '''
 if __name__ == '__main__':
